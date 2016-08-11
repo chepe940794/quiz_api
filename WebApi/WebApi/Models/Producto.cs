7544af240b01fe636bc844b0fdb0548a11b251f3
juan jose <chepe0794@hotmail.com>
2016-08-10 22:13:44 -0600

    quiz

---
+++ WebApi/WebApi/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApi.Models
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int UnidadesEnAlmacen { get; set; }
+    }
+
+    class ProductosDbContext : DbContext
+    {
+         public DbSet<Producto> Productos { get; set; } 
+    }
+}
